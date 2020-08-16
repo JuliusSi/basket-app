b725cb1b55b37245295e4d6d549ad478c9060e91
julius <julius.foqs@gmail.com>
2020-08-16 09:13:50 +0300

    updated translations

--- resources/lang/lt/weather-rules.php
+++ resources/lang/lt/weather-rules.php
@@ -3,7 +3,7 @@
 return [
     'precipitation' => 'galimas lietus, kritulių kiekis: :precipitation mm.',
     'success' => 'Oras tinkamas krepšiniui',
-    'error' => 'Oras netinkamas: ',
+    'error' => 'Oras netinkamas',
     'to_high_air_temperature' => 'per aukšta oro temperatūra: :airTemperature ℃.',
     'to_low_air_temperature' => 'per žema oro temperatūra: :airTemperature ℃.',
 ];
