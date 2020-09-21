39d0ae930bc6e2bc3d5b7c95b7b736b0ee310899
julius <julius.foqs@gmail.com>
2020-09-21 10:30:58 +0300

    removed public folder from gitignore

--- config/weather-example.php
+++ config/weather-example.php
@@ -4,6 +4,7 @@ return [
     'max_precipitation' => 0,
     'max_air_temperature' => 26,
     'min_air_temperature' => 14,
+    'min_air_temperature_if_clear' => 11,
     'hours_to_check' => 4,
     'place_code_to_check' => 'vilnius',
 ];
