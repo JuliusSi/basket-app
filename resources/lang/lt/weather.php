3f868cd9335338826cff590c4f8df553c22a6b8f
julius <julius.foqs@gmail.com>
2021-01-06 23:34:06 +0200

    updated translations

--- resources/lang/lt/weather.php
+++ resources/lang/lt/weather.php
@@ -3,7 +3,7 @@
 return [
     'check_weather' => 'Orų tikrinimas',
     'check_weather_for_basketball' => 'Tikrinti orą krepšiniui',
-    'select_place' => 'Prašome pasirinkti vietą',
-    'select_start_date' => 'Prašome pasirinkti pradžios datą',
-    'select_end_date' => 'Prašome pasirinkti pabaigos datą',
+    'select_place' => 'Pasirinkite vietą',
+    'select_start_date' => 'Pasirinkite pradžios datą',
+    'select_end_date' => 'Pasirinkite pabaigos datą',
 ];
