d8c3ac28d6ceb7b1fe1370338e2f48b665230c80
julius <julius.foqs@gmail.com>
2021-11-29 18:23:29 +0200

    minor chat fixes

--- resources/js/components/ChatMessage.vue
+++ resources/js/components/ChatMessage.vue
@@ -7,7 +7,11 @@
             <div class="chat-message">
                 <span class="text-small">{{ formatDate(message.created_at)  }}</span>
                 <h5>{{ message.user.username }}</h5>
-                <p>{{ message.message }}</p>
+                <p>
+                    <img :class="{ full: fullWidthImage }" @click="fullWidthImage = !fullWidthImage"
+                         v-if="isImagePath(message.message)" :src=message.message alt="image">
+                    <span v-else>{{ message.message }}</span>
+                </p>
             </div>
         </div>
     </div>
@@ -18,14 +22,32 @@ import moment from 'moment';
 
 export default {
     props: ['message'],
+    data() {
+        return {
+            'fullWidthImage': false
+        }
+    },
 
     methods: {
         formatDate(date) {
             moment.locale("lt")
             let isToday = moment(date).isSame(moment().clone().startOf('day'), 'd');
             let isSameHour = moment(date).isSame(moment().clone().startOf('hour'), 'h');
+            if (isToday && isSameHour) {
+                return moment(date).startOf('minute').fromNow()
+            } else if (isToday) {
+                return moment(date).startOf('hour').fromNow()
+            } else {
+                return moment(date).format('lll');
+            }
+        },
+        isImagePath(message) {
+            const acceptedImageExtensions = ['gif', 'jpeg', 'jpg', 'png'];
 
-            return isToday && isSameHour ? moment(date).startOf('minute').fromNow() : moment(date).format('lll');
+            return acceptedImageExtensions.includes(this.getExtension(message))
+        },
+        getExtension(url) {
+            return url.split(/[#?]/)[0].split('.').pop().trim();
         },
     },
 }
