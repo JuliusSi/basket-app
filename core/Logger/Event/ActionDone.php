56ea62f5dda8e16b28cf80f587c8716e8b13c614
julius <julius.foqs@gmail.com>
2022-01-06 21:08:38 +0200

    minor refactor

--- core/Logger/Event/ActionDone.php
+++ core/Logger/Event/ActionDone.php
@@ -5,13 +5,17 @@ declare(strict_types=1);
 namespace Core\Logger\Event;
 
 use Core\Logger\Model\Log;
-use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Bus\Queueable;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 
 class ActionDone
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
 
     public function __construct(public Log $log)
     {
