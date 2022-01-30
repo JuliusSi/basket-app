adaf04a64310a2d6e8ecff7c74d7912ef9f47e83
julius <julius.foqs@gmail.com>
2022-01-31 00:49:12 +0200

    added interface to controller and minor fixes

--- app/Http/Controllers/Api/ChatApiController.php
+++ app/Http/Controllers/Api/ChatApiController.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Api;
 
-use App\Chat\Message\Service\BroadcastAwareMessageSendingService;
+use App\Chat\Message\Service\MessageSendingServiceInterface;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ChatMessageStoreRequest;
 use App\Model\ChatMessage;
@@ -12,16 +12,18 @@ use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 
 class ChatApiController extends Controller
 {
+    public function __construct(private MessageSendingServiceInterface $messageSendService)
+    {
+    }
+
     public function getMessages(): LengthAwarePaginator
     {
         return ChatMessage::with('user:id,username,image_path')->latest()->paginate(8);
     }
 
-    public function sendMessage(
-        ChatMessageStoreRequest $request,
-        BroadcastAwareMessageSendingService $messageSendService
-    ): array {
-        $messageSendService->send($request->user(), $request->get('message'));
+    public function sendMessage(ChatMessageStoreRequest $request): array
+    {
+        $this->messageSendService->send($request->user(), $request->get('message'));
 
         return [];
     }
