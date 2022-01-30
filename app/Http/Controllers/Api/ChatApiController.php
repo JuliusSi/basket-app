ad3c672e9369c90a4d288ce294505b367158832e
julius <julius.foqs@gmail.com>
2022-01-30 21:10:57 +0200

    removed php doc

--- app/Http/Controllers/Api/ChatApiController.php
+++ app/Http/Controllers/Api/ChatApiController.php
@@ -10,27 +10,17 @@ use App\Http\Requests\ChatMessageStoreRequest;
 use App\Model\ChatMessage;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 
-/**
- * Class ChatApiController
- * @package App\Http\Controllers\Api
- */
 class ChatApiController extends Controller
 {
-    /**
-     * @return LengthAwarePaginator
-     */
     public function getMessages(): LengthAwarePaginator
     {
         return ChatMessage::with('user:id,username,image_path')->latest()->paginate(8);
     }
 
-    /**
-     * @param  ChatMessageStoreRequest  $request
-     * @param  BroadcastAwareMessageSendingService  $messageSendService
-     * @return array
-     */
-    public function sendMessage(ChatMessageStoreRequest $request, BroadcastAwareMessageSendingService $messageSendService): array
-    {
+    public function sendMessage(
+        ChatMessageStoreRequest $request,
+        BroadcastAwareMessageSendingService $messageSendService
+    ): array {
         $messageSendService->send($request->user(), $request->get('message'));
 
         return [];
