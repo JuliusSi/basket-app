849186390609ac6bc236840f8802f0efa5c790d2
julius <julius.foqs@gmail.com>
2022-03-28 22:32:16 +0300

    minor refactoring

--- core/Storage/Service/LocalStorageService.php
+++ core/Storage/Service/LocalStorageService.php
@@ -1,40 +1,22 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Core\Storage\Service;
 
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 
-/**
- * Class LocalStorageService
- * @package Core\Storage\Service
- */
 class LocalStorageService
 {
     public const DIRECTORY_MEMES = 'memes';
     public const DIRECTORY_RANDOM = 'random';
     public const DIRECTORY_COURTS = 'courts';
 
-    /**
-     * @var Storage
-     */
-    private Storage $storage;
-
-    /**
-     * LocalStorageService constructor.
-     * @param  Storage  $storage
-     */
-    public function __construct(Storage $storage)
+    public function __construct(private Storage $storage)
     {
-        $this->storage = $storage;
     }
 
-    /**
-     * @param  string  $fileName
-     * @param  string  $directory
-     * @param  string  $disk
-     * @return string|null
-     */
     public function findFileUrl(string $fileName, string $directory, string $disk = 'public'): ?string
     {
         $path = $this->getPath($fileName, $directory, $disk);
@@ -43,24 +25,15 @@ class LocalStorageService
         }
 
         Log::warning(sprintf('File %s not found', $fileName));
+
         return null;
     }
 
-    /**
-     * @param  string  $directory
-     * @return array
-     */
     public function findAllDirectoryFiles(string $directory): array
     {
         return $this->storage::allFiles($directory);
     }
 
-    /**
-     * @param  string  $fileName
-     * @param  string  $directory
-     * @param  string  $disk
-     * @return string
-     */
     private function getPath(string $fileName, string $directory, string $disk): string
     {
         return sprintf('%s/%s/%s', $disk, $directory, $fileName);
