172bade4a3548b8a33139e9bc6798b2245ffa602
julius <julius.foqs@gmail.com>
2022-04-20 23:22:45 +0300

    updated logging

--- src/Weather/Repository/WeatherRepository.php
+++ src/Weather/Repository/WeatherRepository.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Src\Weather\Repository;
 
 use GuzzleHttp\Exception\GuzzleException;
@@ -7,30 +9,16 @@ use Src\Weather\Client\DefaultClient;
 use Src\Weather\Client\Request\DefaultRequest;
 use Src\Weather\Client\Response\Response;
 
-/**
- * Class WeatherRepository
- * @package Src\Weather\Repository
- */
 class WeatherRepository
 {
-    /**
-     * @var DefaultClient
-     */
     private DefaultClient $client;
 
-    /**
-     * WeatherProvider constructor.
-     *
-     * @param  DefaultClient  $client
-     */
     public function __construct(DefaultClient $client)
     {
         $this->client = $client;
     }
 
     /**
-     * @param  string  $placeCode
-     * @return Response|null
      * @throws GuzzleException
      */
     public function find(string $placeCode): ?Response
@@ -38,10 +26,6 @@ class WeatherRepository
         return $this->client->getDeserializedResponse($this->buildRequest($placeCode), Response::class);
     }
 
-    /**
-     * @param  string  $place
-     * @return DefaultRequest
-     */
     private function buildRequest(string $place): DefaultRequest
     {
         $request = new DefaultRequest();
