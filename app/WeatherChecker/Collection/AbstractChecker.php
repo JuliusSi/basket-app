790e6a4952fccc0f352c216aab98a94566c6552a
julius <julius.foqs@gmail.com>
2022-04-23 19:59:49 +0300

    check past weather fix

--- app/WeatherChecker/Collection/AbstractChecker.php
+++ app/WeatherChecker/Collection/AbstractChecker.php
@@ -1,21 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\WeatherChecker\Collection;
 
-/**
- * Class AbstractChecker
- * @package App\WeatherChecker\Collection
- */
 abstract class AbstractChecker implements CheckerInterface
 {
-    /**
-     * @param  string  $date
-     * @param  int  $hour
-     * @param  string  $rule
-     * @return string
-     */
     protected function getKey(string $date, int $hour, string $rule): string
     {
-        return $date . '_' . $hour . '_' . $rule;
+        return $date.'_'.$hour.'_'.$rule;
     }
 }
